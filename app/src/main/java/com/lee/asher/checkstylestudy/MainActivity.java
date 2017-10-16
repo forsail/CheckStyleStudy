6070c5c811ad0618c6d8f8c02d29578e771f2a41
lihong <lihong_sail@163.com>
2017-10-16 10:06:02 +0800

    first commit

---
+++ app/src/main/java/com/lee/asher/checkstylestudy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lee.asher.checkstylestudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(1 > 2) {
+
+        }
+    }
+}
